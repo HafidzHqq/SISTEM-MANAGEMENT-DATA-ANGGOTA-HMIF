8fe87d52cb15f7d3a1f66cd1a26c761db436f84c
HafidzHqq <haqiqihf@gmail.com>
2026-07-02 23:50:29 +0700

    Update site favicon with HMIF logo and link it in the welcome template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sistem Management Data Anggota HMIF</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
 <body>
